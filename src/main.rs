534d43e5f4745763fd3c9bcae490ffec7ebcf7e8
felix <Wu-Felix@outlook.com>
2025-02-25 15:34:53 +0800

    feat: add file search functionality
    - Added `find_git_root_file` method to search for files in git root.
    - Implemented `find_file_with_extension` to recursively search for files
      with a specific extension.
    - Updated imports to include `fs` and `Error`.
    - Added `#[allow(dead_code)]` attribute to `print_git_root` method.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use std::{io::Result, path::PathBuf};
+use std::{
+    fs,
+    io::{Error, Result},
+    path::PathBuf,
+};
 
 use duct::cmd;
 trait FindFile {
@@ -6,6 +10,7 @@ trait FindFile {
         let git_root = cmd!("git", "rev-parse", "--show-toplevel").read()?;
         Ok(PathBuf::from(git_root))
     }
+    #[allow(dead_code)]
     fn print_git_root(&self) {
         if let Ok(git_root) = self.find_git_root() {
             println!("{:#?}", git_root);
@@ -13,7 +18,34 @@ trait FindFile {
             println!("not find git root");
         }
     }
+    fn find_git_root_file(&self, file: &str) -> Result<Vec<PathBuf>> {
+        let git_root_pwd = self.find_git_root()?;
+        let path_list = self.find_file_with_extension(git_root_pwd, file)?;
+        if path_list.is_empty() {
+            return Err(Error::new(
+                std::io::ErrorKind::NotFound,
+                format!("not find {}", file),
+            ));
+        }
+        Ok(path_list)
+    }
+    fn find_file_with_extension(&self, git_root_pwd: PathBuf, file: &str) -> Result<Vec<PathBuf>> {
+        let mut path_list: Vec<PathBuf> = Vec::new();
+        for entry in fs::read_dir(git_root_pwd)? {
+            let entry = entry?;
+            let path = entry.path();
+            if path.is_dir() {
+                path_list.extend(self.find_file_with_extension(path, file)?);
+            } else if let Some(extension) = path.extension() {
+                if extension == file {
+                    path_list.push(path);
+                }
+            }
+        }
+        Ok(path_list)
+    }
 }
+
 struct LazyMdk {}
 impl<U> FindFile for U {}
 fn main() {
