fa61b32a7d3dd54440031ab554469d6540f7e84b
felix <Wu-Felix@outlook.com>
2025-02-25 14:17:55 +0800

    feat: add function to find and print git root directory
    - Implemented `find_git_root` to locate the git root directory using
      `git rev-parse --show-toplevel`.
    - Added `print_git_root` to print the found git root directory.
    - Created `LazyMdk` struct and implemented `FindFile` trait for it.

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,22 @@
-fn main() {}
+use std::{io::Result, path::PathBuf};
+
+use duct::cmd;
+trait FindFile {
+    fn find_git_root(&self) -> Result<PathBuf> {
+        let git_root = cmd!("git", "rev-parse", "--show-toplevel").read()?;
+        Ok(PathBuf::from(git_root))
+    }
+    fn print_git_root(&self) {
+        if let Ok(git_root) = self.find_git_root() {
+            println!("{:#?}", git_root);
+        } else {
+            println!("not find git root");
+        }
+    }
+}
+struct LazyMdk {}
+impl<U> FindFile for U {}
+fn main() {
+    let lazy_mdk = LazyMdk {};
+    lazy_mdk.print_git_root();
+}
